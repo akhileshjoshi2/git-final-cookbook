e16c52bf6fa93717deb138c594c25e48de0b70b6
root <root@workstation.(none)>
2016-08-07 17:59:16 +0000

    first commit

---
+++ environments/dev.rb
@@ -0,0 +1,10 @@
+name "dev"
+description "this is a dev environment"
+cookbook "apache", "= 0.1.5"
+default_attributes({
+	"author" => {
+		"name" => "jeff"
+}
+})
+
+
---
+++ environments/prod.rb
@@ -0,0 +1,3 @@
+name "prod"
+description "producntion environment"
+cookbook "apache","= 0.1.0" 
